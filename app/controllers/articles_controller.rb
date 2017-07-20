cef19c0c513b1013d72bb6b52f1800fcb7db11c5
Nico Pickelny <npickelny@gmail.com>
2017-07-20 14:34:49 -0300

    config circle

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article = Article.find(params[:id])
+    authorize @article
     @article.destroy
 
     redirect_to articles_path
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
+    authorize @comment
     @comment.destroy
     redirect_to article_path(@article)
   end
