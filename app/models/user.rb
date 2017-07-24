8231bd6019e2ae4543fc70708dad5b5d39ffd8e8
Nico Pickelny <npickelny@gmail.com>
2017-07-24 14:31:17 -0300

    config circle ci4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
-
 end
